4388bf1f6376acb829ebb6bd1cd9f07d7f1e95de
Tim <72918879+Painini@users.noreply.github.com>
2022-05-10 12:04:32 +0200

    adding card class in progress :)

---
+++ Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kortspel
+{
+    abstract class Card
+    {
+        int number;
+        string symbol;
+        string color;
+        public Card(int number, string symbol, string color)
+        {
+
+        }
+    }
+}
