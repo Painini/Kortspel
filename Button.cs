70b7cc1efd276bfe5002cc9d2ca2ad763c88c3b1
Painini <72918879+Painini@users.noreply.github.com>
2022-05-22 22:04:27 +0200

    button

--- Button.cs
+++ Button.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Kortspel
 {
     public class Button
     {
-        int height;
-        int width;
+        Vector2 dimensions;
+        Vector2 position;
+        // Fortsätt här sen
+        public Button(Vector2 dimensions, Vector2 position)
+        {
+            this.dimensions = dimensions;
+            this.position = position;
+        }
 
 
 
